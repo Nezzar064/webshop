a8e7aacbe519b0c6b38bbd9d28fc376dd2afab7f
Mathias <math8802@stud.kea.dk>
2022-03-29 18:10:57 +0200

    optimized code for async/await

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -7,7 +7,7 @@ const logger = require('../utils/logger');
 
 const moduleName = 'auth.controller.js';
 
-exports.signUp = (req, res) => {
+exports.signUp = async (req, res) => {
     logger.info(`${moduleName} request to signup user ${JSON.stringify(req.body)}`);
 
     const user = new User({
@@ -15,30 +15,22 @@ exports.signUp = (req, res) => {
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password)
     });
-    try {
-        user.save((err, user) => {
-            if (err) {
-                logger.error(`${moduleName} save user unexpected error ${JSON.stringify(err)}`);
-                res.status(500).send({ message: err });
-                return;
-            }
-    
+
+    await user.save()
+        .then(async (user) => {
+
             // Using roles from request body if present
             if (req.body.roles) {
                 logger.debug(`${moduleName} using roles from req.body for signup ${JSON.stringify(req.body.roles)}`);
-    
-                Role.find(
+
+                await Role.find(
                     {
                         name: { $in: req.body.roles }
-                    },
-                    (err, roles) => {
-                        if (err) {
-                            logger.error(`${moduleName} find roles unexpected error ${JSON.stringify(err)}`);
-                            res.status(500).send({ message: err });
-                            return;
-                        }
+                    })
+                    .exec()
+                    .then(async (roles) => {
                         user.roles = roles.map(role => role._id);
-                        user.save(err => {
+                        await user.save(err => {
                             if (err) {
                                 logger.error(`${moduleName} save user unexpected error ${JSON.stringify(err)}`);
                                 res.status(500).send({ message: err });
@@ -47,66 +39,88 @@ exports.signUp = (req, res) => {
                             logger.info(`${moduleName} user successfully signed up ${JSON.stringify(user)}`);
                             res.status(200).send({ message: 'User successfully signed up!' });
                         });
-                    }
-                );
-            
-            // Else defaulting to user role
+                    })
+                    .catch((err) => {
+                        logger.error(`${moduleName} find roles unexpected error ${JSON.stringify(err)}`);
+                        res.status(500).send({ message: err });
+                        return;
+                    });
+
+                // Else defaulting to user role
             } else {
                 logger.debug(`${moduleName} using default user role for signup`);
-                Role.findOne({ name: "user" }, (err, role) => {
-                    if (err) {
+                await Role.findOne({ name: "user" })
+                    .exec()
+                    .then(async (role) => {
+                        user.roles = [role._id];
+                        await user.save()
+                            .then(() => {
+                                logger.info(`${moduleName} user successfully saved using default role (user)`);
+                                res.status(200).send({ message: "User successfully signed up!" });
+                            })
+                            .catch((err) => {
+                                logger.error(`${moduleName} save user (default role) unexpected error ${JSON.stringify(err)}`);
+                                res.status(500).send({ message: err });
+                                return;
+                            });
+                    })
+                    .catch((err) => {
                         logger.error(`${moduleName} find role unexpected error ${JSON.stringify(err)}`);
                         res.status(500).send({ message: err });
                         return;
-                    }
-                    user.roles = [role._id];
-                    user.save(err => {
-                        if (err) {
-                            logger.error(`${moduleName} save user (default role) unexpected error ${JSON.stringify(err)}`);
-                            res.status(500).send({ message: err });
-                            return;
-                        }
-                        logger.info(`${moduleName} user successfully saved using default role (user)`);
-                        res.status(200).send({ message: "User successfully signed up!" });
                     });
-                });
             }
+        })
+        .catch((err) => {
+            logger.error(`${moduleName} unexpected error during sign-up ${JSON.stringify(err)}`);
+            return res.status(500).end();
         });
-    } catch (err) {
-        logger.error(`${moduleName} unexpected error during sign-up ${JSON.stringify(err)}`);
-        return res.status(500).end();
-    }
 };
 
-exports.signIn = (req, res) => {
-    try {
-        User.findOne({
-            username: req.body.username
-        })
+exports.signIn = async (req, res) => {
+    await User.findOne({
+        username: req.body.username
+    })
         .populate('roles', '-__v')
-        .exec(async (err, user) => {
-            if (err) {
-                logger.error(`${moduleName} find user (signin) unexpected error ${JSON.stringify(err)}`);
-                res.status(500).send({ message: err });
+        .exec()
+        .then(async (user) => {
+            if (!user) {
+                logger.error(`${moduleName} user not present in database`);
+                res.status(400).send({ message: 'User could not be found!' });
                 return;
             }
-    
+
+            // Create access token upon login
             let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
                 expiresIn: parseInt(process.env.ACCESS_TOKEN_EXPIRY),
             });
-    
-            let refreshToken = await RefreshToken.createRefreshToken(user);
+
+            // Find refresh token if it exists, otherwise create a new one.
+            let refreshToken = RefreshToken.findOne({ user: user._id })
+                .exec(async (err, refreshToken) => {
+                    if (!err) {
+                        logger.error(`${moduleName} refresh-token handler error ${JSON.stringify(err)}`);
+                        return null;
+                    }
+
+                    if (!refreshToken) {
+                        logger.info(`${moduleName} created new refresh token for user ${JSON.stringify(user._id)}`);
+                        return await RefreshToken.createRefreshToken(user);
+                    }
+                    logger.info(`${moduleName} returned users existing token ${JSON.stringify(user._id)}`);
+                    return refreshToken;
+                });
             if (!refreshToken) {
                 throw new Error('Refresh Token error');
             }
-    
+
             let _roles = [];
             user.roles.forEach(role => {
                 _roles.push(`ROLE_${role.name.toString().toUpperCase()}`);
             });
-            
+
             logger.info(`${moduleName} user successfully authenticated ${JSON.stringify(user)}`);
-    
+
             res.status(200).send({
                 id: user._id,
                 username: user.username,
@@ -115,26 +129,27 @@ exports.signIn = (req, res) => {
                 accessToken: token,
                 refreshToken: refreshToken,
             });
+
+        })
+        .catch((err) => {
+            logger.error(`${moduleName} unexpected error during sign-in ${JSON.stringify(err)}`);
+            return res.status(500).end();
         });
-    } catch (err) {
-        logger.error(`${moduleName} unexpected error during sign-in ${JSON.stringify(err)}`);
-        return res.status(500).end();
-    }
 };
 
 exports.refreshToken = async (req, res) => {
     const { refreshToken: requestToken } = req.body;
     if (!requestToken) {
         logger.error(`${moduleName} refreshToken empty body`);
-        return res.status(403).send({message: 'Refresh token is required'});
+        return res.status(403).send({ message: 'Refresh token is required' });
     }
 
-    try {
-        // Try to find token, send appropiate message if not found
-        let refreshToken = await RefreshToken.findOne({ token: requestToken });
+    await RefreshToken.findOne({ token: requestToken })
+    .exec()
+    .then(async (refreshToken) => {
         if (!refreshToken) {
             logger.error(`${moduleName} Refresh Token not present in database`);
-            res.status(403).send({message: 'Refresh token not present in database'});
+            res.status(403).send({ message: 'Refresh token could not be found!' });
             return;
         }
 
@@ -142,22 +157,23 @@ exports.refreshToken = async (req, res) => {
         if (RefreshToken.verifyRtExpiration(refreshToken)) {
             logger.info(`${moduleName} Refresh Token is expired, requested for new login`);
 
-            RefreshToken.findByIdAndRemove(refreshToken._id, { useFindAndModify: false});
-            res.status(403).send({message: 'Refresh token is expired, please sign in again.'});
+            await RefreshToken.findByIdAndRemove(refreshToken._id, { useFindAndModify: false });
+            res.status(403).send({ message: 'Refresh token is expired, please sign in again.' });
             return;
         }
 
         // Generate new access token
-        let _accessToken = jwt.sign({ id: refreshToken.user._id}, process.env.SECRET_KEY, { 
+        let _accessToken = jwt.sign({ id: refreshToken.user._id }, process.env.SECRET_KEY, {
             expiresIn: parseInt(process.env.ACCESS_TOKEN_EXPIRY),
         });
-        
+
         return res.status(200).send({
             accessToken: _accessToken,
             refreshToken: refreshToken.token,
-        });
-    } catch (err) {
+        });  
+    })
+    .catch((err) => {
         logger.info(`${moduleName} Refresh Token unexpected error ${JSON.stringify(err)}`);
         return res.status(500).end();
-    }
+    });
 };
--- auth/middlewares/guards.js
+++ auth/middlewares/guards.js
@@ -7,102 +7,109 @@ const logger = require('../utils/logger');
 const moduleName = 'guards.js -';
 
 // Used for verifying request with guards
-verifyTokenWithGuard = (req, res, next) => {
-    try {
-        let token = getToken(req);
+verifyTokenWithGuard = async (req, res, next) => {
+    let token = getToken(req);
 
-        // Return 403 if token is not provided
-        if (!token) {
-            logger.error(`${moduleName} no token provided`);
-            res.status(403).send({ message: 'No token was provided' });
-            return;
-        }
+    // Return 403 if token is not provided
+    if (!token) {
+        logger.error(`${moduleName} no token provided`);
+        res.status(403).send({ message: 'No token was provided' });
+        return;
+    }
 
-        // Verify token
-        jwt.verify(token, process.env.SECRET_KEY, async (err, decoded) => {
-            if (err) {
+    // Verify token
+    await jwt.verify(token, process.env.SECRET_KEY)
+        .then(decoded => {
+            if (!decoded) {
                 logger.error(`${moduleName} token is invalid ${JSON.stringify(err)}`);
                 res.status(401).send({ message: 'Token not valid - Unauthorized!' });
                 return;
             }
-            if (decoded) {
-                logger.info(`${moduleName} Token successfully verified`);
-                req.userId = decoded.id;
-                console.log(decoded.id);
-                next();
+            logger.info(`${moduleName} Token successfully verified`);
+            req.userId = decoded.id;
+            next();
+        })
+        .catch((err) => {
+            if (err instanceof TokenExpiredError) {
+                logger.error(`${moduleName} access token is expired`);
+                res.status(401).send({ message: 'Access token was expired - Unauthorized!' });
+                return;
             }
+            logger.error(`${moduleName} error validating token ${JSON.stringify(err)}`);
+            return res.status(500).end();
         });
-
-    } catch (err) {
-        logger.error(`${moduleName} error validating token ${JSON.stringify(err)}`);
-        return res.status(500).end();
-    }
 };
 
 // Admin guard
-adminGuard = (req, res, next) => {
-    User.findById(req.userId).exec((err, user) => {
-        if (err) {
-            logger.error(`${moduleName} adminGuard find user error ${JSON.stringify(err)}`);
-            res.status(500).send({ message: err });
-            return;
-        }
-        Role.find(
-            {
-                _id: { $in: user.roles }
-            },
-            (err, roles) => {
-                if (err) {
+adminGuard = async (req, res, next) => {
+    await User.findById(req.userId)
+        .exec()
+        .then(async (user) => {
+            await Role.find(
+                {
+                    _id: { $in: user.roles }
+                },
+            )
+                .exec()
+                .then(roles => {
+                    for (let i = 0; i < roles.length; i++) {
+                        if (roles[i].name === 'admin') {
+                            logger.debug(`${moduleName} adminGuard user authorized to access this resource`);
+                            next();
+                            return;
+                        }
+                    }
+                    logger.debug(`${moduleName} adminGuard user not authorized to access this resource`);
+                    res.status(403).send({ message: 'Admin role required - Unauthorized!' });
+                    return;
+                })
+                .catch((err) => {
                     logger.error(`${moduleName} adminGuard find roles error ${JSON.stringify(err)}`);
                     res.status(500).send({ message: err });
                     return;
-                }
-                for (let i = 0; i < roles.length; i++) {
-                    if (roles[i].name === 'admin') {
-                        logger.debug(`${moduleName} adminGuard user authorized to access this resource`);
-                        next();
-                        return;
-                    }
-                }
-                logger.debug(`${moduleName} adminGuard user not authorized to access this resource`);
-                res.status(403).send({ message: 'Admin role required - Unauthorized!' });
-                return;
-            }
-        );
-    });
+                });
+        })
+        .catch((err) => {
+            logger.error(`${moduleName} adminGuard find user error ${JSON.stringify(err)}`);
+            res.status(500).send({ message: err });
+            return;
+        });
 };
 
 // Mod guard
-modGuard = (req, res, next) => {
-    User.findById(req.userId).exec((err, user) => {
-        if (err) {
-            logger.error(`${moduleName} modGuard find user error ${JSON.stringify(err)}`);
-            res.status(500).send({ message: err });
-            return;
-        }
-        Role.find(
-            {
-                _id: { $in: user.roles }
-            },
-            (err, roles) => {
-                if (err) {
+modGuard = async (req, res, next) => {
+    await User.findById(req.userId)
+        .exec()
+        .then(async (user) => {
+            await Role.find(
+                {
+                    _id: { $in: user.roles }
+                },
+            )
+                .exec()
+                .then((roles) => {
+                    for (let i = 0; i < roles.length; i++) {
+                        if (roles[i].name === 'moderator') {
+                            logger.debug(`${moduleName} modGuard user authorized to access this resource`);
+                            next();
+                            return;
+                        }
+                    }
+                    logger.debug(`${moduleName} modGuard user not authorized to access this resource`);
+                    res.status(403).send({ message: 'Moderator role required - Unauthorized!' });
+                    return;
+                })
+                .catch((err) => {
                     logger.error(`${moduleName} modGuard find roles error ${JSON.stringify(err)}`);
                     res.status(500).send({ message: err });
                     return;
-                }
-                for (let i = 0; i < roles.length; i++) {
-                    if (roles[i].name === 'moderator') {
-                        logger.debug(`${moduleName} modGuard user authorized to access this resource`);
-                        next();
-                        return;
-                    }
-                }
-                logger.debug(`${moduleName} modGuard user not authorized to access this resource`);
-                res.status(403).send({ message: 'Moderator role required - Unauthorized!' });
-                return;
-            }
-        );
-    });
+                });
+        })
+        .catch((err) => {
+            logger.error(`${moduleName} modGuard find user error ${JSON.stringify(err)}`);
+            res.status(500).send({ message: err });
+            return;
+        });
 };
 
 const jwtUtil = {
